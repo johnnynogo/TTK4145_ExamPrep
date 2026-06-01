054fbecdea55dc9a0cae8fa5312bfbaf2f3b5749
johnnynogo <nguyenjohnnyngo@gmail.com>
2026-06-01 12:43:51 +0200

    fixed a minor mistake on line 29 EndReading, had semicolong instead of 'is'

--- realtime_final_exam_prep/manyReadersSingleWriter/manyreaderssinglewriterada/src/manyreaderssinglewriterada.adb
+++ realtime_final_exam_prep/manyReadersSingleWriter/manyreaderssinglewriterada/src/manyreaderssinglewriterada.adb
@@ -10,27 +10,28 @@ procedure Main is
    end ManyReadersSingleWriter;
 
    protected body ManyReadersSingleWriter is
-   entry StartWriting when not Writing and Readers = 0 is          -- TODO: WANT NOT WRITING AND READERS == 0?
-   begin
-      Writing := True;
-   end StartWriting;
 
-   procedure EndWriting is
-   begin
-      Writing := False;
-   end EndWriting;
+      entry StartWriting when not Writing and Readers = 0 is
+      begin
+         Writing := True;
+      end StartWriting;
 
-   entry StartReading when not Writing is
-   begin
-      Readers := Readers + 1;
-   end StartReading;
+      procedure EndWriting is
+      begin
+         Writing := False;
+      end EndWriting;
 
-   procedure EndReading;
-   begin
-      Readers := Readers - 1;
-   end EndReading;
+      entry StartReading when not Writing is
+      begin
+         Readers := Readers + 1;
+      end StartReading;
 
-   end ManyReadersSingleWriter; 
+      procedure EndReading is
+      begin
+         Readers := Readers - 1;
+      end EndReading;
+
+   end ManyReadersSingleWriter;
 
 begin
    null;
